89814295b8db85461901a09f5281866970142671
Nguyen Thanh Huy <huymau170819999@gmail.com>
2024-07-30 22:53:00 +0700

    update product Id

--- src/Page/ProductDetailPage.tsx
+++ src/Page/ProductDetailPage.tsx
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom";
 import useFetchProduct from "../CustomHooks/fetchProduct";
 import { TypeDataProduct } from "../store/reducer/productReducers";
+import { FaStar } from "react-icons/fa";
 
 interface TypeDetail extends TypeDataProduct {
   rating: object;
@@ -9,24 +10,43 @@ interface TypeDetail extends TypeDataProduct {
 const ProductDetailPage = () => {
   const param = useParams();
   // console.log( "Param: ",`products/${param.id}`);
-  const { data }: TypeDetail[] = useFetchProduct(`products/${param.id}`);
+  const { data, loading }: TypeDetail[] = useFetchProduct(
+    `products/${param.id}`
+  );
   // const data = useSelector((state : RootState)=> state.productData)
   console.log("Huy Detail Page", data);
+  console.log("Huy Detail Page", loading);
   return (
     <div className="flex items-center justify-center">
-      <div className="my-20 w-[70vw] h-[70vh] flex items-center 
-      shadow-xl shadow-emerald-500 bg-white rounded-full gap-5">
+      <div
+        className="my-20 w-[70vw] h-[70vh] flex items-center 
+      shadow-xl shadow-emerald-500 bg-white rounded-full gap-5"
+      >
         <div className="ml-[8rem]">
           <img src={data?.image} className="max-w-[250px]" />
         </div>
 
-        <div className="">
-        <h3 className="text-2xl font-bold text-neutral-500">{data?.title}</h3>
-        <p className="text-neutral-400">{data?.description}</p>
-        <p className="text-3xl text-emerald-500 font-semibold">{data?.price}$</p>
+        <div className="mx-5 flex flex-col gap-4">
+          <h3 className="text-2xl font-bold text-neutral-500">{data?.title}</h3>
+          <p className="text-neutral-400">{data?.description}</p>
+          <p className="text-3xl text-emerald-500 font-semibold">
+            {data?.price}$
+          </p>
+          <p className="text-neutral-600">
+            The remaining amount :{" "}
+            <span className="text-emerald-500 font-semibold">
+              {" "}
+              {data?.rating?.count}
+            </span>
+          </p>
+          <p className="text-neutral-600 flex gap-2">
+            Star :
+            <span className="text-emerald-500 font-semibold flex items-baseline">
+              {data?.rating?.rate} <FaStar />
+            </span>
+          </p>
+        </div>
       </div>
-      </div>
-      
     </div>
   );
 };
